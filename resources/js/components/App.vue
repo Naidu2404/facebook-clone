dff861f300d3f5df9cd3da3dd20cd63788daa1bc
Naidu2404 <venkateswaranaidumoyya@gmail.com>
2024-02-13 19:23:26 +0530

    added nav sidebar newpost and newsfeed

--- resources/js/components/App.vue
+++ resources/js/components/App.vue
@@ -1,17 +1,27 @@
 <template>
-    <div>
-        Start
+    <div class="flex flex-col flex-1 h-screen overflow-y-hidden">
+        <Nav />
 
-        <router-view></router-view>
+        <div class="flex overflow-y-hidden flex-1">
+            <SideBar />
+            <div class="overflow-x-hidden w-2/3">
+                <router-view></router-view>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-import router from "../router";
+import Nav from "./Nav.vue";
+import SideBar from "./SideBar.vue";
 
 export default {
     name: "App",
-    components: { router },
+
+    components: {
+        Nav,
+        SideBar,
+    },
 };
 </script>
 
--- resources/js/router.js
+++ resources/js/router.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
-import Start from "./views/Start.vue";
+import NewsFeed from "./views/NewsFeed.vue";
 
 const routes = [
     {
         path: "/",
         name: "home",
-        component: Start,
+        component: NewsFeed,
     },
 ];
 
