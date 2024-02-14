1688958b03cad7dda7e85270d49dbe728c1e587a
Naidu2404 <venkateswaranaidumoyya@gmail.com>
2024-02-14 16:12:22 +0530

    AddedPost component

--- resources/js/views/NewsFeed.vue
+++ resources/js/views/NewsFeed.vue
@@ -2,20 +2,20 @@
     <div class="flex flex-col items-center py-4">
         <NewPost />
 
-        <!-- <Post /> -->
+        <Post />
     </div>
 </template>
 
 <script>
 import NewPost from "../components/NewPost.vue";
-// import Post from "../components/Post";
+import Post from "../components/Post.vue";
 
 export default {
     name: "NewsFeed",
 
     components: {
         NewPost,
-        // Post,
+        Post,
     },
 };
 </script>
